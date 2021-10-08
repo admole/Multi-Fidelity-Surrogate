d0f88bf6047544c4f8e67dcd46754cbb80e4b2d6
Andrew Mole <andrew.mole@postgrad.manchester.ac.uk>
2021-10-08 18:26:35 +0100

    Set the turbulence model from json

--- Cases/setup.py
+++ Cases/setup.py
@@ -15,14 +15,22 @@ from PyFoam.Basics.DataStructures import Vector
 # from PyFoam.Basics.DataStructures import UnparsedList
 
 
+def create_case(case):
+    shutil.copytree("Base", case['name'])
+    set_model(case)
+
+
+def set_model(case):
+    turbfile = ParsedParameterFile(path.join(SolutionDirectory(case['name']).constantDir(), "turbulenceProperties"))
+    turbfile['RAS']['RASModel'] = case['RASModel']
+    # turbfile["actions"].clear()
+    # turbfile["actions"].append(haloDict)
+    turbfile.writeFile()
+
+
 # Opening JSON file
 j = open(path.join(os.getcwd(), "cases.json"))  # Check this works elsewhere
 cases = json.load(j)
-
-
-def create_case(case):
-    shutil.copytree("Base", case['name'])
-
 
 for i in range(len(cases)):
     print(f"Setting up {cases[i]['name']}")
