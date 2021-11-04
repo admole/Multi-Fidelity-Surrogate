2a4efaeb8706c256d4a123b18f31f457ea6ebdad
Andrew Mole <andrew.mole@postgrad.manchester.ac.uk>
2021-11-04 15:21:32 +0000

    turn mfGP into function and use with yaw plots

--- Plots/yaw.py
+++ Plots/yaw.py
@@ -10,38 +10,60 @@ import fonts
 import json
 import forces
 import cf
+import mfGP
 
 
-def plot_yaw(model, ax, col):
+def get_yaw(model):
     # Opening JSON file
     j = open(os.path.join(os.getcwd(), f"../Data/{model}/Yaw/inlet_sweep.json"))
     case_settings = json.load(j)
     # case_settings = [{"Name": "test11"}]
     numcases = len(case_settings)
     path = f"{model}/Yaw/"
-    cd = np.zeros(shape=(2, numcases))
-    recirc = np.zeros(numcases)
+    cd1 = np.zeros(numcases)
+    cd2 = np.zeros(numcases)
+    # recirc = np.zeros(numcases)
     alpha = np.zeros(numcases)
     for i in range(numcases):
         case = os.path.join(path, case_settings[i]["Name"])
         alpha[i] = case_settings[i]["FlowAngle"]
-        cd[0, i] = forces.get_cd(case, 'cube1')
-        cd[1, i] = forces.get_cd(case, 'cube2')
+        cd1[i] = forces.get_cd(case, 'cube1')
+        cd2[i] = forces.get_cd(case, 'cube2')
         # recirc[i] = cf.recirculation(case)
         # print(recirc[i])
-
-    ax.plot(alpha, cd[0], f'{col}.', markersize=12, label=f'{model} cube1')
-    ax.plot(alpha, cd[1], f'{col}+', markersize=12, label=f'{model} cube2')
-    ax.set_xlabel(r'$\alpha$')
-    ax.set_ylabel(r'Cd')
-    ax.set_ylim(bottom=0)
-    ax.set_xlim(left=0)
-    ax.legend(frameon=False, ncol=2)
+    data = {r'$\alpha$': alpha, r'$Cd_1$': cd1, r'$Cd_2$': cd2}
+    return data
 
 
-fig1, axes1 = plt.subplots(1, 1, figsize=(6, 3), squeeze=False, constrained_layout=True, sharex=True, sharey=True)
-plot_yaw("RANS", axes1[0, 0], 'r')
-plot_yaw("LES", axes1[0, 0], 'b')
+def plot_yaw(ax, rans, les, variable):
+    ax.plot(rans[r'$\alpha$'], rans[variable], 'r.', markersize=12, label=f'RANS Sample')
+    alpha, rans_mean, rans_std, les_mean, les_std, mf_mean, mf_std = mfGP.mfgp(rans[r'$\alpha$'], rans[variable],
+                                                                               les[r'$\alpha$'], les[variable])
+    ax.plot(alpha, rans_mean, 'r', label='RANS GP Mean')
+    ax.fill_between(alpha[:, 0], rans_mean[:, 0] - rans_std, rans_mean[:, 0] + rans_std, alpha=0.2, color='r')
+    ax.plot(les[r'$\alpha$'], les[variable], 'b.', markersize=12, label=f'LES Sample')
+    ax.plot(alpha, les_mean, 'b', label='LES GP Mean')
+    ax.fill_between(alpha[:, 0], les_mean[:, 0] - les_std, les_mean[:, 0] + les_std, alpha=0.2, color='b')
+    ax.plot(alpha, mf_mean, 'k', label='Multi-fidelity GP Mean')
+    ax.fill_between(alpha[:, 0], mf_mean[:, 0] - mf_std, mf_mean[:, 0] + mf_std,
+                    alpha=0.2, color='k', label="Model +/- 1 std")
+
+    if ax == axes1[-1, 0]:
+        ax.set_xlabel(r'$\alpha$')
+    ax.set_ylabel(variable)
+    ax.set_ylim(0.9*min(mf_mean), 1.1*max(mf_mean))
+    ax.set_xlim(0, max(alpha))
+    ax.legend(frameon=False, ncol=3)
+
+
+variables = [r'$Cd_1$', r'$Cd_2$']
+fig1, axes1 = plt.subplots(len(variables), 1, figsize=(10, 3*len(variables)),
+                           squeeze=False, constrained_layout=True, sharex=True, sharey=False)
+RANS_data = get_yaw('RANS')
+LES_data = get_yaw('LES')
+for it in range(len(variables)):
+    quantity = variables[it]
+    plot_yaw(axes1[it, 0], RANS_data, LES_data, quantity)
 
 plt.show()
 # fig1.savefig('figures/Ubulk_time.pdf', bbox_inches='tight')
