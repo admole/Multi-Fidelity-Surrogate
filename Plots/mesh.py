dc2c0dd145e03fd46d454262f50f6f66433c9a37
Andrew Mole <andrew.mole@postgrad.manchester.ac.uk>
2021-11-04 09:53:12 +0000

    Added mesh resolution plot with Ncells

--- Plots/mesh.py
+++ Plots/mesh.py
@@ -6,10 +6,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 import os
 import sys
-import fonts
 import json
+import re
+import fonts
 import forces
 import cf
+
+
+def get_ncells(c):
+    file = f'../Data/{c}/log.checkMesh'
+    regexp = re.compile(r'^\s*cells.*?(\d+)')
+    n = 0
+    with open(file) as f:
+        for line in f:
+            match = regexp.match(line)
+            if match:
+                n = float(match.group(1))
+                print(f'Retrieved number of cells = {n}')
+    return n
 
 
 # Opening JSON file
@@ -26,10 +40,27 @@ fig1, axes1 = plt.subplots(1, numcases, figsize=(numcases*5, 3.5),
 fig2, axes2 = plt.subplots(1, numcases, figsize=(numcases*5, 3.5),
                            squeeze=False, constrained_layout=True, sharex=True, sharey=True)
 
+fig3, axes3 = plt.subplots(1, 1, figsize=(5, 3.5),
+                           squeeze=False, constrained_layout=True, sharex=True, sharey=True)
+
+
+ncells = np.zeros(numcases)
+cd1 = np.zeros(numcases)
+cd2 = np.zeros(numcases)
 for i in range(numcases):
     case = os.path.join(path, case_settings[i]["Name"])
     forces.plot_forces(case, axes1[0, i])
     cf.plot_cf(case, axes2[0, i])
+    ncells[i] = get_ncells(case)
+    cd1[i] = forces.get_cd(case, 'cube1')
+    cd2[i] = forces.get_cd(case, 'cube2')
+
+axes3[0, 0].plot(ncells, cd1, '.r', markersize=12, label='cube1')
+axes3[0, 0].plot(ncells, cd2, '.b', markersize=12, label='cube2')
+axes3[0, 0].set_xlabel('Number of cells')
+axes3[0, 0].set_ylabel('Cd')
+axes3[0, 0].set_ylim(bottom=0)
+axes3[0, 0].legend(frameon=True)
 
 plt.show()
 # fig1.savefig('figures/Ubulk_time.pdf', bbox_inches='tight')
