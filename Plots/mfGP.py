26065eb442bc168efe0fed37cf51e0f62c0935f9
Andrew Mole <andrew.mole@postgrad.manchester.ac.uk>
2021-11-05 09:31:30 +0000

    transform to 0 mean and unit variance and invert after

--- Plots/mfGP.py
+++ Plots/mfGP.py
@@ -2,6 +2,7 @@ from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import (RBF, Matern, RationalQuadratic,
                                               ExpSineSquared, DotProduct,
                                               ConstantKernel)
+from sklearn import preprocessing
 import numpy as np
 
 
@@ -17,17 +18,29 @@ def mfgp(x_lf, lf, x_hf, hf):
     lf = lf.reshape(-1, 1)
     hf = hf.reshape(-1, 1)
 
+    lf_scaler = preprocessing.StandardScaler().fit(lf)
+    lf = lf_scaler.transform(lf)
+    hf_scaler = preprocessing.StandardScaler().fit(hf)
+    hf = hf_scaler.transform(hf)
+
     gpr_hf = GaussianProcessRegressor(kernel=RBF(), n_restarts_optimizer=2000).fit(x_hf, hf)
-    gpr_lf = GaussianProcessRegressor(kernel=RBF(), n_restarts_optimizer=200).fit(x_lf, lf)
+    gpr_lf = GaussianProcessRegressor(kernel=RBF(), n_restarts_optimizer=200).fit(x_lf, lf)  # convergence warning
 
     l1mean = gpr_lf.predict(x_hf)
     l2_train = np.hstack((x_hf, l1mean))
-    gpr_mf_l2 = GaussianProcessRegressor(kernel=RBF() * RBF() + RBF(), n_restarts_optimizer=200).fit(l2_train, hf)
+    gpr_mf_l2 = GaussianProcessRegressor(kernel=RBF() * RBF() + RBF(), n_restarts_optimizer=200).fit(l2_train, hf)  # convergence warning
 
     pred_hf_mean, pred_hf_std = gpr_hf.predict(x, return_std=True)
     pred_lf_mean, pred_lf_std = gpr_lf.predict(x, return_std=True)
 
     l2_test = np.hstack((x, pred_lf_mean))
     pred_mf_mean, pred_mf_std = gpr_mf_l2.predict(l2_test, return_std=True)
+
+    pred_lf_mean = lf_scaler.inverse_transform(pred_lf_mean)
+    pred_hf_mean = hf_scaler.inverse_transform(pred_hf_mean)
+    pred_mf_mean = hf_scaler.inverse_transform(pred_mf_mean)
+    pred_lf_std *= lf_scaler.scale_
+    pred_hf_std *= hf_scaler.scale_
+    pred_mf_std *= hf_scaler.scale_
 
     return x, pred_lf_mean, pred_lf_std, pred_hf_mean, pred_hf_std, pred_mf_mean, pred_mf_std
