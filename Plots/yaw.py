7bfd9743146a62f98557c6b6e9e526ccdfe4bae9
Andrew Mole <andrew.mole@postgrad.manchester.ac.uk>
2021-11-15 17:03:58 +0000

    Plot differentiates untrained and trained LES points

--- Plots/yaw.py
+++ Plots/yaw.py
@@ -37,12 +37,14 @@ def get_yaw(model):
 
 
 def plot_yaw(ax, rans, les, variable):
-    ax.plot(rans[r'$\alpha$'], rans[variable], 'r.', markersize=12, label=f'RANS Sample')
+    les_train = les[les[r"$\alpha$"] % 10 == 0]
+    ax.scatter(rans[r'$\alpha$'], rans[variable], c='r', label=f'RANS Sample')
     alpha, rans_mean, rans_std, les_mean, les_std, mf_mean, mf_std = mfGP.mfgp(rans[r'$\alpha$'].to_numpy(), rans[variable].to_numpy(),
-                                                                               les[r'$\alpha$'].to_numpy(), les[variable].to_numpy())
+                                                                               les_train[r'$\alpha$'].to_numpy(), les_train[variable].to_numpy())
     ax.plot(alpha, rans_mean, 'r', label='RANS GP Mean')
     ax.fill_between(alpha[:, 0], rans_mean[:, 0] - rans_std, rans_mean[:, 0] + rans_std, alpha=0.2, color='r')
-    ax.plot(les[r'$\alpha$'], les[variable], 'b.', markersize=12, label=f'LES Sample')
+    ax.scatter(les[r'$\alpha$'], les[variable], edgecolors='b', facecolors='none')
+    ax.scatter(les_train[r'$\alpha$'], les_train[variable], c='b', label=f'LES Sample')
     ax.plot(alpha, les_mean, 'b', label='LES GP Mean')
     ax.fill_between(alpha[:, 0], les_mean[:, 0] - les_std, les_mean[:, 0] + les_std, alpha=0.2, color='b')
     ax.plot(alpha, mf_mean, 'k', label='Multi-fidelity GP Mean')
@@ -52,17 +54,15 @@ def plot_yaw(ax, rans, les, variable):
     if ax == axes1[-1, 0]:
         ax.set_xlabel(r'$\alpha$')
     ax.set_ylabel(variable)
-    ax.set_ylim(0.9*min(mf_mean), 1.1*max(mf_mean))
     ax.set_xlim(0, max(alpha))
-    ax.legend(frameon=False, ncol=3)
+    ax.legend(frameon=False, ncol=2)
 
 
 variables = [r'$Cd_1$', r'$Cd_2$']
 fig1, axes1 = plt.subplots(len(variables), 1, figsize=(10, 3*len(variables)),
-                           squeeze=False, constrained_layout=True, sharex=True, sharey=False)
+                           squeeze=False, constrained_layout=True, sharex='all', sharey='none')
 RANS_data = get_yaw('RANS')
 LES_data = get_yaw('LES')
-LES_data = LES_data[LES_data[r"$\alpha$"] % 10 != 0]
 for it in range(len(variables)):
     quantity = variables[it]
     plot_yaw(axes1[it, 0], RANS_data, LES_data, quantity)
