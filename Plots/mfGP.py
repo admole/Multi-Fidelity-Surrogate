f14b37450fcac4879a9fd788e44468dd5c2b3678
Andrew Mole <andrew.mole@postgrad.manchester.ac.uk>
2021-11-17 13:48:57 +0000

    Fix lengthscale to that used by lf for hf and mf

--- Plots/mfGP.py
+++ Plots/mfGP.py
@@ -1,9 +1,12 @@
 from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.gaussian_process.kernels import (RBF, Matern, RationalQuadratic,
                                               ExpSineSquared, DotProduct,
-                                              ConstantKernel)
+                                              ConstantKernel, WhiteKernel,
+                                              CompoundKernel, Kernel,
+                                              Product, Sum)
 from sklearn import preprocessing
 import numpy as np
+import pprint
 
 
 def mfgp(x_lf, lf, x_hf, hf):
@@ -18,28 +21,25 @@ def mfgp(x_lf, lf, x_hf, hf):
     lf = lf.reshape(-1, 1)
     hf = hf.reshape(-1, 1)
 
-    scaler = preprocessing.StandardScaler().fit(np.concatenate((lf, hf)))
-    lf = scaler.transform(lf)
-    hf = scaler.transform(hf)
+    k_lf = RBF()
+    # k_lf = RationalQuadratic()
 
-    gpr_hf = GaussianProcessRegressor(kernel=RBF(), n_restarts_optimizer=2000).fit(x_hf, hf)
-    gpr_lf = GaussianProcessRegressor(kernel=RBF(), n_restarts_optimizer=200).fit(x_lf, lf)  # convergence warning
+    gpr_lf = GaussianProcessRegressor(kernel=k_lf, n_restarts_optimizer=200, normalize_y=True).fit(x_lf, lf)
+    pprint.pprint(gpr_lf.kernel_.get_params())
+    length = gpr_lf.kernel_.get_params()['length_scale']
+    k_hf = RBF(length, length_scale_bounds='fixed')
+    gpr_hf = GaussianProcessRegressor(kernel=k_hf, n_restarts_optimizer=2000, normalize_y=True).fit(x_hf, hf)
+
 
     l1mean = gpr_lf.predict(x_hf)
     l2_train = np.hstack((x_hf, l1mean))
-    gpr_mf_l2 = GaussianProcessRegressor(kernel=RBF() * RBF() + RBF(), n_restarts_optimizer=200).fit(l2_train, hf)  # convergence warning
+    k_mf = k_hf  # * k_hf + k_hf
+    gpr_mf_l2 = GaussianProcessRegressor(kernel=k_mf, n_restarts_optimizer=200, normalize_y=True).fit(l2_train, hf)
 
     pred_hf_mean, pred_hf_std = gpr_hf.predict(x, return_std=True)
     pred_lf_mean, pred_lf_std = gpr_lf.predict(x, return_std=True)
 
     l2_test = np.hstack((x, pred_lf_mean))
     pred_mf_mean, pred_mf_std = gpr_mf_l2.predict(l2_test, return_std=True)
-
-    pred_lf_mean = scaler.inverse_transform(pred_lf_mean)
-    pred_hf_mean = scaler.inverse_transform(pred_hf_mean)
-    pred_mf_mean = scaler.inverse_transform(pred_mf_mean)
-    pred_lf_std *= scaler.scale_
-    pred_hf_std *= scaler.scale_
-    pred_mf_std *= scaler.scale_
 
     return x, pred_lf_mean, pred_lf_std, pred_hf_mean, pred_hf_std, pred_mf_mean, pred_mf_std
