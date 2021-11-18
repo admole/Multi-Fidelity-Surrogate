ee7d5941620eb48a363dfa2e64d19e6652dcf688
Andrew Mole <andrew.mole@postgrad.manchester.ac.uk>
2021-11-18 15:53:07 +0000

    Refactor MLPR hyperparameters

--- Plots/mfGP.py
+++ Plots/mfGP.py
@@ -11,7 +11,6 @@ import pprint
 
 
 def mfgp(x_lf, lf, x_hf, hf):
-
     xmin = min(min(x_lf), min(x_hf))
     xmax = max(max(x_lf), max(x_hf))
     x = np.linspace(xmin, xmax, 1000)[:, np.newaxis]
@@ -22,28 +21,40 @@ def mfgp(x_lf, lf, x_hf, hf):
     lf = lf.reshape(-1, 1)
     hf = hf.reshape(-1, 1)
 
-    # k_lf = RBF()
-    # k_lf = RationalQuadratic()
+    solver = 'lbfgs'
+    activation = 'relu'
+    hidden_layers = (100)
 
-    gpr_lf = MLPRegressor(random_state=1, solver='lbfgs', max_iter=2000).fit(x_lf, lf)
-    # pprint.pprint(gpr_lf.kernel_.get_params())
-    # length = gpr_lf.kernel_.get_params()['length_scale']
-    # k_hf = RBF(length, length_scale_bounds='fixed')
-    gpr_hf = MLPRegressor(random_state=1, solver='lbfgs', max_iter=2000).fit(x_hf, hf)
+    mlpr_lf = MLPRegressor(activation=activation,
+                           hidden_layer_sizes=hidden_layers,
+                           solver=solver,
+                           random_state=1,
+                           shuffle=True,
+                           max_iter=1000).fit(x_lf, lf)
+    mlpr_hf = MLPRegressor(activation=activation,
+                           hidden_layer_sizes=hidden_layers,
+                           solver=solver,
+                           random_state=1,
+                           shuffle=True,
+                           max_iter=1000).fit(x_hf, hf)
 
-    l1mean = gpr_lf.predict(x_hf)
+    l1mean = mlpr_lf.predict(x_hf)
     l1mean = l1mean.reshape(-1, 1)
     l2_train = np.hstack((x_hf, l1mean))
-    # k_mf = k_hf  # * k_hf + k_hf
-    gpr_mf_l2 = MLPRegressor(random_state=1, solver='lbfgs', max_iter=2000).fit(l2_train, hf)
+    mlpr_mf_l2 = MLPRegressor(activation=activation,
+                              hidden_layer_sizes=hidden_layers,
+                              solver=solver,
+                              random_state=1,
+                              shuffle=True,
+                              max_iter=1000).fit(l2_train, hf)
 
-    pred_hf_mean = gpr_hf.predict(x)
-    pred_lf_mean = gpr_lf.predict(x)
+    pred_hf_mean = mlpr_hf.predict(x)
+    pred_lf_mean = mlpr_lf.predict(x)
 
     pred_lf_mean = pred_lf_mean.reshape(-1, 1)
     pred_hf_mean = pred_hf_mean.reshape(-1, 1)
     l2_test = np.hstack((x, pred_lf_mean))
-    pred_mf_mean = gpr_mf_l2.predict(l2_test)
+    pred_mf_mean = mlpr_mf_l2.predict(l2_test)
     pred_mf_mean = pred_mf_mean.reshape(-1, 1)
 
     pred_lf_std = np.zeros(len(pred_lf_mean))
