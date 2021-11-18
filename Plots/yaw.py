632b29f1d6dbddaadb2bbc450aaa9efd9c1f4096
Andrew Mole <andrew.mole@postgrad.manchester.ac.uk>
2021-11-18 16:37:17 +0000

    Renaming multifidelity regression file

--- Plots/yaw.py
+++ Plots/yaw.py
@@ -10,7 +10,7 @@ import fonts
 import json
 import forces
 import cf
-import mfGP
+import mfRegression as mfr
 
 
 def get_yaw(model):
@@ -39,8 +39,10 @@ def get_yaw(model):
 def plot_yaw(ax, rans, les, variable):
     les_train = les[les[r"$\alpha$"] % 10 == 0]
     ax.scatter(rans[r'$\alpha$'], rans[variable], c='r', label=f'RANS Sample')
-    alpha, rans_mean, rans_std, les_mean, les_std, mf_mean, mf_std = mfGP.mfgp(rans[r'$\alpha$'].to_numpy(), rans[variable].to_numpy(),
-                                                                               les_train[r'$\alpha$'].to_numpy(), les_train[variable].to_numpy())
+    alpha, rans_mean, rans_std, les_mean, les_std, mf_mean, mf_std = mfr.mfgp(rans[r'$\alpha$'].to_numpy(),
+                                                                              rans[variable].to_numpy(),
+                                                                              les_train[r'$\alpha$'].to_numpy(),
+                                                                              les_train[variable].to_numpy())
     ax.plot(alpha, rans_mean, 'r', label='RANS GP Mean')
     ax.fill_between(alpha[:, 0], rans_mean[:, 0] - rans_std, rans_mean[:, 0] + rans_std, alpha=0.2, color='r')
     ax.scatter(les[r'$\alpha$'], les[variable], edgecolors='b', facecolors='none')
