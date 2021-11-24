8cdd3056e99b3b99271f4e0472eb14658656a2a6
Andrew Mole <andrew.mole@postgrad.manchester.ac.uk>
2021-11-24 15:16:19 +0000

    Add shifts to multi-fidelity regression (embedding theory)
    from https://doi.org/10.1016/j.jcp.2019.109020
    and https://doi.org/10.1098/rsfs.2018.0083

--- Plots/mfRegression.py
+++ Plots/mfRegression.py
@@ -27,14 +27,18 @@ def mfgp(x_lf, lf, x_hf, hf):
     gpr_hf = GaussianProcessRegressor(kernel=k_hf, n_restarts_optimizer=2000, normalize_y=True).fit(x_hf, hf)
 
     l1mean = gpr_lf.predict(x_hf)
-    l2_train = np.hstack((x_hf, l1mean))
+    l1mean_shift1 = gpr_lf.predict(x_hf+0.04)
+    l1mean_shift2 = gpr_lf.predict(x_hf+0.08)
+    l2_train = np.hstack((x_hf, l1mean, l1mean_shift1, l1mean_shift2))
     k_mf = k_hf * k_hf + k_hf
     gpr_mf_l2 = GaussianProcessRegressor(kernel=k_mf, n_restarts_optimizer=200, normalize_y=True).fit(l2_train, hf)
 
     pred_hf_mean, pred_hf_std = gpr_hf.predict(x, return_std=True)
     pred_lf_mean, pred_lf_std = gpr_lf.predict(x, return_std=True)
+    pred_lf_mean_shift1, pred_lf_std_shift1 = gpr_lf.predict(x+0.04, return_std=True)
+    pred_lf_mean_shift2, pred_lf_std_shift2 = gpr_lf.predict(x+0.08, return_std=True)
 
-    l2_test = np.hstack((x, pred_lf_mean))
+    l2_test = np.hstack((x, pred_lf_mean, pred_lf_mean_shift1, pred_lf_mean_shift2))
     pred_mf_mean, pred_mf_std = gpr_mf_l2.predict(l2_test, return_std=True)
 
     return x, pred_lf_mean, pred_lf_std, pred_hf_mean, pred_hf_std, pred_mf_mean, pred_mf_std
@@ -64,6 +68,7 @@ def mfmlp(x_lf, lf, x_hf, hf):
 
     solver = 'lbfgs'
     activation = 'tanh'
+    activation = 'relu'
     hidden_layers = (20, 50, 20)
 
     mlpr_lf = MLPRegressor(activation=activation,
@@ -79,16 +84,19 @@ def mfmlp(x_lf, lf, x_hf, hf):
                            shuffle=True,
                            max_iter=1000).fit(x_hf, hf)
 
-    l1mean = mlpr_lf.predict(x_hf)
-    l1mean = l1mean.reshape(-1, 1)
-    l2_train = np.hstack((x_hf, l1mean))
+    l1mean = mlpr_lf.predict(x_hf).reshape(-1, 1)
+    l1mean_shift1 = mlpr_lf.predict(x_hf+0.04).reshape(-1, 1)
+    l1mean_shift2 = mlpr_lf.predict(x_hf+0.08).reshape(-1, 1)
+    # l2_train = np.hstack((x_hf, l1mean))
+    l2_train = np.hstack((x_hf, l1mean, l1mean_shift1, l1mean_shift2))
+
 
     mlpr_mf_lin = MLPRegressor(activation='identity',
                                hidden_layer_sizes=hidden_layers,
                                solver=solver,
                                random_state=1,
                                shuffle=True,
-                               alpha=0.1,
+                               alpha=0.001,
                                max_iter=1000)
 
     mlpr_mf_nlin = MLPRegressor(activation=activation,
@@ -102,12 +110,12 @@ def mfmlp(x_lf, lf, x_hf, hf):
     models = [('lin', mlpr_mf_lin), ('nlin', mlpr_mf_nlin)]
     stacking = StackingRegressor(estimators=models, cv=4, passthrough=True).fit(l2_train, hf)
 
-    pred_hf_mean = mlpr_hf.predict(x)
-    pred_lf_mean = mlpr_lf.predict(x)
+    pred_hf_mean = mlpr_hf.predict(x).reshape(-1, 1)
+    pred_lf_mean = mlpr_lf.predict(x).reshape(-1, 1)
+    pred_lf_mean_shift1 = mlpr_lf.predict(x+0.04).reshape(-1, 1)
+    pred_lf_mean_shift2 = mlpr_lf.predict(x+0.08).reshape(-1, 1)
 
-    pred_lf_mean = pred_lf_mean.reshape(-1, 1)
-    pred_hf_mean = pred_hf_mean.reshape(-1, 1)
-    l2_test = np.hstack((x, pred_lf_mean))
+    l2_test = np.hstack((x, pred_lf_mean, pred_lf_mean_shift1, pred_lf_mean_shift2))
     pred_mf_mean = stacking.predict(l2_test)
     pred_mf_mean = pred_mf_mean.reshape(-1, 1)
 
