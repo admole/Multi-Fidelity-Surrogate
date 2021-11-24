1c5bae53f9c7d915b31af349142a819ce1c5a857
Andrew Mole <andrew.mole@postgrad.manchester.ac.uk>
2021-11-24 18:17:20 +0000

    Remove stacking regressor

--- Plots/mfRegression.py
+++ Plots/mfRegression.py
@@ -68,20 +68,19 @@ def mfmlp(x_lf, lf, x_hf, hf):
 
     solver = 'lbfgs'
     activation = 'tanh'
-    activation = 'relu'
+    # activation = 'relu'
     hidden_layers = (20, 50, 20)
+    hidden_layers = (20, 20)
 
     mlpr_lf = MLPRegressor(activation=activation,
                            hidden_layer_sizes=hidden_layers,
                            solver=solver,
                            random_state=1,
-                           shuffle=True,
                            max_iter=1000).fit(x_lf, lf)
     mlpr_hf = MLPRegressor(activation=activation,
                            hidden_layer_sizes=hidden_layers,
                            solver=solver,
                            random_state=1,
-                           shuffle=True,
                            max_iter=1000).fit(x_hf, hf)
 
     l1mean = mlpr_lf.predict(x_hf).reshape(-1, 1)
@@ -90,25 +89,12 @@ def mfmlp(x_lf, lf, x_hf, hf):
     # l2_train = np.hstack((x_hf, l1mean))
     l2_train = np.hstack((x_hf, l1mean, l1mean_shift1, l1mean_shift2))
 
-
-    mlpr_mf_lin = MLPRegressor(activation='identity',
-                               hidden_layer_sizes=hidden_layers,
-                               solver=solver,
-                               random_state=1,
-                               shuffle=True,
-                               alpha=0.001,
-                               max_iter=1000)
-
     mlpr_mf_nlin = MLPRegressor(activation=activation,
                                 hidden_layer_sizes=hidden_layers,
                                 solver=solver,
                                 random_state=1,
-                                shuffle=True,
-                                alpha=0.0001,
-                                max_iter=1000)
-
-    models = [('lin', mlpr_mf_lin), ('nlin', mlpr_mf_nlin)]
-    stacking = StackingRegressor(estimators=models, cv=4, passthrough=True).fit(l2_train, hf)
+                                # alpha=0.0001,
+                                max_iter=1000).fit(l2_train, hf)
 
     pred_hf_mean = mlpr_hf.predict(x).reshape(-1, 1)
     pred_lf_mean = mlpr_lf.predict(x).reshape(-1, 1)
@@ -116,7 +102,7 @@ def mfmlp(x_lf, lf, x_hf, hf):
     pred_lf_mean_shift2 = mlpr_lf.predict(x+0.08).reshape(-1, 1)
 
     l2_test = np.hstack((x, pred_lf_mean, pred_lf_mean_shift1, pred_lf_mean_shift2))
-    pred_mf_mean = stacking.predict(l2_test)
+    pred_mf_mean = mlpr_mf_nlin.predict(l2_test)
     pred_mf_mean = pred_mf_mean.reshape(-1, 1)
 
     pred_lf_std = np.zeros(len(pred_lf_mean))
