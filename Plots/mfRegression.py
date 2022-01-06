591c46d160973149736a9d098a67d501350e7aa4
Andrew Mole <andrew.mole@postgrad.manchester.ac.uk>
2022-01-06 13:05:12 +0000

    Use Matern for GPR and define MFMLP layers seperately

--- Plots/mfRegression.py
+++ Plots/mfRegression.py
@@ -19,17 +19,20 @@ def mfgp(x_lf, lf, x_hf, hf):
     lf = lf.reshape(-1, 1)
     hf = hf.reshape(-1, 1)
 
-    k_lf = RBF()
+    k_lf = Matern()
+    # k_lf = RBF()
 
     gpr_lf = GaussianProcessRegressor(kernel=k_lf, n_restarts_optimizer=200, normalize_y=True).fit(x_lf, lf)
     pprint.pprint(gpr_lf.kernel_.get_params())
-    k_hf = RBF()
+    k_hf = Matern()
+    # k_hf = RBF()
     gpr_hf = GaussianProcessRegressor(kernel=k_hf, n_restarts_optimizer=2000, normalize_y=True).fit(x_hf, hf)
 
     l1mean = gpr_lf.predict(x_hf)
     l1mean_shift1 = gpr_lf.predict(x_hf+0.02)
     l1mean_shift2 = gpr_lf.predict(x_hf-0.02)
     l2_train = np.hstack((x_hf, l1mean, l1mean_shift1, l1mean_shift2))
+    # l2_train = np.hstack((x_hf, l1mean))
     k_mf = k_hf * k_hf + k_hf
     gpr_mf_l2 = GaussianProcessRegressor(kernel=k_mf, n_restarts_optimizer=200, normalize_y=True).fit(l2_train, hf)
 
@@ -39,6 +42,7 @@ def mfgp(x_lf, lf, x_hf, hf):
     pred_lf_mean_shift2, pred_lf_std_shift2 = gpr_lf.predict(x-0.02, return_std=True)
 
     l2_test = np.hstack((x, pred_lf_mean, pred_lf_mean_shift1, pred_lf_mean_shift2))
+    # l2_test = np.hstack((x, pred_lf_mean))
     pred_mf_mean, pred_mf_std = gpr_mf_l2.predict(l2_test, return_std=True)
 
     return x, pred_lf_mean, pred_lf_std, pred_hf_mean, pred_hf_std, pred_mf_mean, pred_mf_std
@@ -75,16 +79,19 @@ def mfmlp(x_lf, lf, x_hf, hf):
     solver = 'lbfgs'
     activation = 'tanh'
     # activation = 'relu'
-    hidden_layers = (20, 50, 20)
-    hidden_layers = (20, 20, 20, 20)
+    hidden_layers1 = (20, 50, 50, 50, 20)
+    hidden_layers2 = (20, 20, 20, 20)
+
+    # hidden_layers1 = (20, 20, 20, 20)
+    # hidden_layers2 = (20, 20, 20, 20)
 
     mlpr_lf = MLPRegressor(activation=activation,
-                           hidden_layer_sizes=hidden_layers,
+                           hidden_layer_sizes=hidden_layers1,
                            solver=solver,
                            random_state=1,
                            max_iter=1000).fit(x_lf, lf)
     mlpr_hf = MLPRegressor(activation=activation,
-                           hidden_layer_sizes=hidden_layers,
+                           hidden_layer_sizes=hidden_layers2,
                            solver=solver,
                            random_state=1,
                            max_iter=1000).fit(x_hf, hf)
@@ -92,12 +99,11 @@ def mfmlp(x_lf, lf, x_hf, hf):
     l1mean = mlpr_lf.predict(x_hf).reshape(-1, 1)
     l1mean_shift1 = mlpr_lf.predict(x_hf+0.02).reshape(-1, 1)
     l1mean_shift2 = mlpr_lf.predict(x_hf+0.04).reshape(-1, 1)
-    # l2_train = np.hstack((x_hf, l1mean))
     l2_train = np.hstack((x_hf, l1mean, l1mean_shift1, l1mean_shift2))
     # l2_train = np.hstack((x_hf, l1mean))
 
     mlpr_mf_nlin = MLPRegressor(activation=activation,
-                                hidden_layer_sizes=hidden_layers,
+                                hidden_layer_sizes=hidden_layers2,
                                 solver=solver,
                                 random_state=1,
                                 # alpha=0.0001,
