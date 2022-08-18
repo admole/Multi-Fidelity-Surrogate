7138fd03c789406280d48ee2c1418b95273754bd
Andrew Mole <andrew.mole@postgrad.manchester.ac.uk>
2022-08-18 08:17:03 +0100

    Define surface plane fr plotting fields on

--- Plots/fields.py
+++ Plots/fields.py
@@ -62,13 +62,12 @@ def add_cubes(ax, normal='y'):
 
 
 def plot_surface(ax, data, field, angle):
+    normal = 'z' if SURFACE[0] == 'y' else 'y'
     magnitude = np.sqrt(data[f'{field}_x']**2 + data[f'{field}_y']**2 + data[f'{field}_z']**2)
-    # contour = ax.tricontourf(data['x'], data['z'], data['UMean_x'], cmap='seismic', levels=np.arange(-1, 1.5, 0.1))
-    # contour = ax.tricontourf(data['x'], data['z'], magnitude, cmap='bwr', levels=np.arange(0, 2, 0.05))
-    contour = ax.tricontourf(data['x'], data['z'], magnitude, cmap='inferno', levels=np.arange(0, 1.5, 0.05))
-    # contour = ax.tricontour(data['x'], data['z'], data[f'{field}_x'], colors='w', linewidths=1, levels=[0])
+    contour = ax.tricontourf(data['x'], data[normal], magnitude, cmap='inferno', levels=np.arange(0, 1.5, 0.05))
+    # contour = ax.tricontour(data['x'], data[normal], data[f'{field}_x'], colors='k', linewidths=1, levels=[0])
     ax.set_aspect('equal')
-    add_cubes(ax)
+    add_cubes(ax, SURFACE[0])
     ax.annotate(fr'$\theta = {angle}^\circ$', xy=(0.2, 3), xytext=(0.2, 3), size=fonts.BIG_SIZE)
     return contour
 
@@ -80,15 +79,15 @@ def draw(it, ax1, ax2, angles):
     if any(x == ang for x in RANS_CASES):
         print('Plotting RANS velocity')
         acase = f'RANS/Yaw/a{ang}'
-        velocity = get_surface(acase, field='UMean', surface='yNormal')
+        velocity = get_surface(acase, field='UMean', surface=SURFACE)
         contour = plot_surface(ax1, velocity, 'UMean', ang)
 
     if any(x == ang for x in LES_CASES):
         print('Plotting LES velocity')
         acase = f'LES/Yaw/a{ang}'
-        velocity = get_surface(acase, field='U', surface='yNormal')
+        velocity = get_surface(acase, field='U', surface=SURFACE)
         contour = plot_surface(ax2[0], velocity, 'U', ang)
-        velocity = get_surface(acase, field='UMean', surface='yNormal')
+        velocity = get_surface(acase, field='UMean', surface=SURFACE)
         contour = plot_surface(ax2[1], velocity, 'UMean', ang)
 
     return contour
@@ -96,7 +95,11 @@ def draw(it, ax1, ax2, angles):
 
 RANS_CASES = [0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40]
 LES_CASES = [0, 5, 10, 15, 20, 25, 30]
+RANS_CASES = [0, 2, 4,]
+LES_CASES = [0,]
 ALL_CASES = list(set(RANS_CASES) | set(LES_CASES))
+SURFACE = 'yNormal'  # 'yHalf' 'zNormal'
+# contours on z-normal plane showing strong grid artifacts (maybe triangulation)
 
 
 def main():
@@ -122,8 +125,9 @@ def main():
             les_plot_position = axes2[les_loc, :]
             contour = draw(i, rans_plot_position, les_plot_position, ALL_CASES)
         for i in range(7):
-            axes[i, 0].set_ylabel(r'$z$')
-            axes2[i, 0].set_ylabel(r'$z$')
+            normal = 'z' if SURFACE[0] == 'y' else 'y'
+            axes[i, 0].set_ylabel(rf'${normal}$')
+            axes2[i, 0].set_ylabel(rf'${normal}$')
         for i in range(3):
             axes[-1, i].set_xlabel(r'$x$')
         axes2[-1, 0].set_xlabel(r'$x$')
@@ -134,8 +138,8 @@ def main():
         cbar2.set_label(r'$u_{mag}/U_0$')
 
         plt.show()
-        fig.savefig('figures/velocityslicesRANS.png', bbox_inches='tight')
-        fig2.savefig('figures/velocityslicesLES.png', bbox_inches='tight')
+        fig.savefig(f'figures/velocityslices-{SURFACE}-RANS.png', bbox_inches='tight')
+        fig2.savefig(f'figures/velocityslices-{SURFACE}-LES.png', bbox_inches='tight')
 
 
 if __name__ == "__main__":
